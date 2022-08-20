f38a12e06d6cdb09a370cb456e7d7d665c9d8af9
ejcarranzap <ejcarranzap@gmail.com>
2022-08-19 19:37:07 -0600

    all

---
+++ dist/App/Process/ad_tab_btn_getfieldProcess.js
@@ -0,0 +1,43 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ad_tab_btn_getcolProcess = void 0;
+class ad_tab_btn_getcolProcess {
+    constructor(app) {
+        this.app = app;
+        this.getDBObjects = (data) => __awaiter(this, void 0, void 0, function* () {
+            var app = this.app;
+            try {
+                yield app.db.sequelize.query('SELECT fn_generate_field(\'' + data.data.ad_tab_id + '\',\'' + data.action.name + '\');', {});
+                return { success: true, data: [] };
+            }
+            catch (e) {
+                console.log(e.message);
+                throw new Error(e.stack);
+            }
+        });
+    }
+    run(params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            /*console.log('ad_table_btn_getcolProcess action called...', params)*/
+            return yield this.generateCols(params);
+        });
+    }
+    generateCols(params) {
+        return __awaiter(this, void 0, void 0, function* () {
+            var me = this;
+            var ds = yield me.getDBObjects(params);
+            return ds;
+        });
+    }
+}
+exports.ad_tab_btn_getcolProcess = ad_tab_btn_getcolProcess;
+//# sourceMappingURL=ad_tab_btn_getfieldProcess.js.map
