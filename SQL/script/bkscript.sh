272ea0cb8eca31d6062138937f66181fd891d2b2
ejcarranzap <ejcarranzap@gmail.com>
2022-11-04 19:16:20 -0600

    all

--- SQL/script/bkscript.sh
+++ SQL/script/bkscript.sh
@@ -1,6 +1,5 @@
 echo generating backup
 now=`date +%d%b%Y.backup` 
-name="../Btest$now"
-echo "../test$now"
+name="../test$now"
 /usr/lib/postgresql/14/bin/pg_dump -U admin -p 5436 -h localhost test > "$name"
 echo complete backup
